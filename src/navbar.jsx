56bd10644141faf2de2da948b5a51eaf2077ce76
pooja1099 <poojasangwan0510@gmail.com>
2023-07-13 22:49:49 +0530

    done

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import './App.css';
 
 const Navbar = () => {
   return (
     <>
         <nav className='container'>
-        <div className='content' style={{display:'inline-block'}}>
+        <div className='content' >
             
-            <p><Link className="nav-link" to="/main">Home</Link></p>
-            <p><Link className="nav-link" to="/About">About</Link></p>
-            <p><Link className="nav-link" to="/ContactUs">ContactUs</Link></p>
-            <p><Link className="nav-link" to="/Data">Data</Link></p>
+            <p id='mytext'><Link className="nav-link" to="/main">Home</Link></p>
+            <p id='mytext'><Link className="nav-link" to="/About">About</Link></p>
+            <p id='mytext'><Link className="nav-link" to="/ContactUs">ContactUs</Link></p>
+            <p id='mytext'><Link className="nav-link" to="/Data">Data</Link></p>
         </div>
         </nav>
     </>
